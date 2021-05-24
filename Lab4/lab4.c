f00bf4519a932a9e0508064352b148aaf460f6b1
FomenkoVD <fom.stud@yandex.ru>
2021-05-24 13:58:14 +0500

    Labs

--- Lab4/lab4.c
+++ Lab4/lab4.c
@@ -1,33 +1,43 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+
+double koren(double a)
+{
+    double b;
+    if (a > 0)
+        b = pow(a, 1.0 / 3.0);
+    else
+        b = -pow(fabs(a), 1.0 / 3.0);
+    return b;
+}
+
+double integ(long int n)
+{
+    double f = 0.0f, x, h = 3.0f / n;
+    for (int i = 0; i < n; i++)
+    {
+        x = -2.0 + i * h + h / 2.0f;
+        if (x <= 0)
+            f += x * x * sin(koren(x) - 3);
+        else
+            f += sqrt(x) * cos(2 * x);
+    }
+    return f * h;
+}
+
 int main()
 {
-    double f, h, x, sum, sum2 = 1, e, del = 0;
-	  int i, n = 4;
-	  printf("Vvedute tochnost` -> e:\n");
-	  scanf ("%le", &e);
-	  printf("\tx\t f(x)\n");
-	  printf("_____________________________\n");
-	  do
-	  {
-	      for (i = 0; i < n; i++)
-	      {
-		        h = 3.0 / n;
-		        x = (i * h + (h / 2.0));
-	          if (x >= -2.0 && x <= 0.0)
-			          f = x * x * sin(pow(x, 1.0/3) - 3.0);
-		        else
-			          f = sqrt(x) * cos(2 * x);
-		        sum += f;
-	      }
-	      printf("kol-vo - > %d\n", n);
-	      sum = sum * h;
-	      del = fabs(sum2 - sum) / 3.0;
-	      sum2 = sum;
-	      n = 2 * n;
-	      printf("summa -> %.10lf\n", sum);
-	      printf("del -> %lf\n", del);
-	  }
-	  while (del > e);
-	  return 0;
+    double In, I2n, e, otvet = 2.42417f;
+    long int n = 1;
+    printf("Vvedite tochnost -> ");
+    scanf("%lf", &e);
+    In = I2n = integ(n);
+    do
+    {
+        In = I2n;
+        n *= 2;
+        I2n = integ(n);
+        printf("n = %ld   I = %lf  D = %lf   \n\n", n, I2n, fabs(In - I2n) / 3);
+    } while ((fabs(In - I2n) / 3) >= e);
+    printf(" \n otvet = %lf \n  In = %lf \n I2n = %lf \n", otvet, In, I2n);
 }
--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
+unsigned int Recursion(unsigned int a, unsigned int b)
+{
+    return b ? Recursion(++a, --b) : a;
+}
+
+unsigned int Cycle(unsigned int i, unsigned int a, unsigned int b)
+{
+    for (i = 0; i < (a + b); i++);
+    return i;
+}
+
 int main()
 {
     int a, b, i;
+    i = 0;
     printf("Введите первое число a ->\n");
     scanf("%d", &a);
     printf("Введите второе число b ->\n");
     scanf("%d", &b);
-    for (i = 0; i < (a + b); i++)
-    {
-    }
-    printf("Сумма a + b -> %d", i);
+    printf("Сумма через рекурсию -> %d\n", Recursion(a ,b));
+    printf("Сумма через цикл -> %d\n", Cycle(i, a, b));
     return 0;
 }
-asdasd
