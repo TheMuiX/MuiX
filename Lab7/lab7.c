c1e594543a6c6d2bfd126bab3c35c5375dc620a1
Alexey <plaxinrus@yandex.ru>
2021-06-14 17:08:12 +0500

    lab7 ready

--- Lab7/lab7.c
+++ Lab7/lab7.c
@@ -4,8 +4,8 @@
 
 void fill(int n, int m, int A[n][m])
 {
-    int i,j;
-    printf("Начальная матрица: \n");
+    int i, j;
+    printf("Starting matrix: \n");
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
@@ -18,27 +18,44 @@ void Exchange(int n, int m, int A[n][m])
     int i, j, b;
     if (n > m)
     {
-        for (i = 0; i < m; i++)
+        for (i = 0; i < n; i++)
+        {
             for (j = 0; j < m; j++)
             if (i == j)
             {
                 b = A[i][j];
-                A[i][j] = A[i][j];
-                A[i][j] = b;
+                A[i][j] = A[i][m - 1 - j];
+                A[i][m - 1 - j] = b;
             }
+        }
     }
-    else
+    if (n < m)
     {
         for (i = 0; i < n; i++)
+        {
             for (j = 0; j < n; j++)
             if (i == j)
             {
                 b = A[i][j];
-                A[i][j] = A[i][j];
-                A[i][j] = b;
+                A[i][j] = A[i][m - 1 - j];
+                A[i][m - 1 - j] = b;
             }
+        }
     }
-    printf("Матрица, диагонали которые поменялись местами: \n");
+    if (n == m)
+    {
+        for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                if (i == j)
+                {
+                    b = A[i][j];
+                    A[i][j] = A[i][n - 1 - j];
+                    A[i][n - 1 - j] = b;
+                }
+            }   
+    }
+    printf("Final matrix: \n");
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < m; j++)
@@ -49,13 +66,14 @@ void Exchange(int n, int m, int A[n][m])
     }
 }
 
+
 int main ()
 {
     srand(time(NULL));
     int n, m;
-    printf("Кол-во строк n -> ");
+    printf("Number of lines n -> ");
     scanf("%d", &n);
-    printf("Кол-во столбцов m -> ");
+    printf("Number of columns m -> ");
     scanf("%d", &m);
     int A[n][m];
     fill(n, m, A);
